5b7ca55d7da8768e3ad26f93287207b0089794eb
Md. Rubel 1764 <md.rubel@bracits.com>
2024-03-01 05:54:12 +0600

    Implement LeaveRequestRepository

--- HR.LeaveManagement.Application/Contracts/Persistence/ILeaveRequestRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/ILeaveRequestRepository.cs
@@ -4,6 +4,8 @@ namespace HR.LeaveManagement.Application.Contracts.Persistence
 {
     public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
     {
-
+        Task<LeaveRequest> GetLeaveRequestWithDetails(int id);
+        Task<List<LeaveRequest>> GetLeaveRequestWithDetails();
+        Task<List<LeaveRequest>> GetLeaveRequestWithDetails(string userId);
     }
 }
--- HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
+++ HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -1,6 +1,7 @@
 ﻿using HR.LeaveManagement.Application.Contracts.Persistence;
 using HR.LeaveManagement.Domain;
 using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace HR.LeaveManagement.Persistence.Repositories
 {
@@ -8,6 +9,24 @@ namespace HR.LeaveManagement.Persistence.Repositories
     {
         public LeaveRequestRepository(HrDatabaseContext context) : base(context)
         {
+        }
+
+        public async Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+        {
+            var leaveRequest = await _context.LeaveRequests.Include(x => x.LeaveType).FirstOrDefaultAsync(x => x.Id == id);
+            return leaveRequest;
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestWithDetails()
+        {
+            var leaveRequest = await _context.LeaveRequests.Include(x => x.LeaveType).ToListAsync();
+            return leaveRequest;
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestWithDetails(string userId)
+        {
+            var leaveRequest = await _context.LeaveRequests.Where(x => x.RequestingEmployeeId == userId).Include(x => x.LeaveType).ToListAsync();
+            return leaveRequest;
         }
     }
 }
