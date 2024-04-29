835f4a8b0fe7c147db1f1e5442d661d27527f5a1
rubel islam <rubelislam301@gmail.com>
2024-04-30 03:45:43 +0600

    Add Project reference in Api Project
    And Add Service Percestent

--- HR.LeaveManagement.Api/Program.cs
+++ HR.LeaveManagement.Api/Program.cs
@@ -1,6 +1,15 @@
+using HR.LeaveManagement.Application;
+using HR.LeaveManagement.Infrastructure;
+using HR.LeaveManagement.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddApplicationServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
