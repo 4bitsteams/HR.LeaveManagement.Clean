b1aecab66e406fb27ed2205dca0a628b608f2595
rubel islam <rubelislam301@gmail.com>
2024-05-16 02:41:42 +0600

    Add GetAllLeaveAllocations Command

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetAllLeaveAllocations/GetLeaveAllocationsQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Logging;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetAllLeaveAllocations;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveAllocations;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocations.Queries.GetAllLeaveAllocations
+{
+    public class GetLeaveAllocationssQueryHandler : IRequestHandler<GetLeaveAllocationsQuery, List<LeaveAllocationDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveAllocationRepository _leaveAllocationsRepository;
+        private readonly IAppLogger<GetLeaveAllocationssQueryHandler> _logger;
+
+        public GetLeaveAllocationssQueryHandler(IMapper mapper, ILeaveAllocationRepository leaveAllocationsRepository, IAppLogger<GetLeaveAllocationssQueryHandler> logger)
+        {
+            this._mapper = mapper;
+            this._leaveAllocationsRepository = leaveAllocationsRepository;
+            this._logger = logger;
+        }
+        public async Task<List<LeaveAllocationDto>> Handle(GetLeaveAllocationsQuery request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Leave Allocations were retrieved successfully");
+            return _mapper.Map<List<LeaveAllocationDto>>(await _leaveAllocationsRepository.GetAsync());
+        }
+    }
+}
