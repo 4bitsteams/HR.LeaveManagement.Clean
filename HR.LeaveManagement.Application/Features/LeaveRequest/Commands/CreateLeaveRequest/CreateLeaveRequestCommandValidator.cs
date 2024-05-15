3514b1bf08f34302f9d877c4d0447ab837623371
rubel islam <rubelislam301@gmail.com>
2024-05-16 05:35:52 +0600

    Create LeaveRequest Crude

---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Commands/CreateLeaveRequest/CreateLeaveRequestCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CreateLeaveRequest
+{
+    public class CreateLeaveRequestCommandValidator : AbstractValidator<CreateLeaveRequestCommand>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public CreateLeaveRequestCommandValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            Include(new BaseLeaveRequestValidator(_leaveTypeRepository));
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Queries/GetLeaveRequestList/GetLeaveRequestListQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestList
+{
+    public class GetLeaveRequestListQuery : IRequest<List<LeaveRequestListDto>>
+    {
+    }
+}
