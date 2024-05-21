f422f2a51e2563485901bd038b5f322cf24c2acb
Md. Rubel 1764 <md.rubel@bracits.com>
2024-05-22 00:02:14 +0600

    Api Client Setup Done

--- HR.LeaveManagement.BlazorUI/Program.cs
+++ HR.LeaveManagement.BlazorUI/Program.cs
@@ -1,4 +1,5 @@
 using HR.LeaveManagement.BlazorUI;
+using HR.LeaveManagement.BlazorUI.Services.Base;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:7260"));
 await builder.Build().RunAsync();
---
+++ HR.LeaveManagement.BlazorUI/Services/Base/BaseHttpService.cs
@@ -0,0 +1,12 @@
+﻿namespace HR.LeaveManagement.BlazorUI.Services.Base
+{
+    public class BaseHttpService
+    {
+        protected readonly IClient _client;
+
+        public BaseHttpService(IClient client)
+        {
+            _client = client;
+        }
+    }
+}
