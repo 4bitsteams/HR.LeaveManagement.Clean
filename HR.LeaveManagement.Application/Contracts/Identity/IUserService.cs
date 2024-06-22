dfdd8df46f84ba6a1756f192106b7682b75b4d84
Md. Rubel 1764 <md.rubel@bracits.com>
2024-06-22 22:56:39 +0600

    Add Intrigration Test

--- HR.LeaveManagement.Application/Contracts/Identity/IUserService.cs
+++ HR.LeaveManagement.Application/Contracts/Identity/IUserService.cs
@@ -6,5 +6,7 @@ namespace HR.LeaveManagement.Application.Contracts.Identity
     {
         Task<List<Employee>> GetEmployees();
         Task<Employee> GetEmployee(string userId);
+
+        public string UserId { get; }
     }
 }
--- HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
+++ HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using HR.LeaveManagement.Domain;
+﻿using HR.LeaveManagement.Application.Contracts.Identity;
+using HR.LeaveManagement.Domain;
 using HR.LeaveManagement.Domain.Common;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,9 +7,11 @@ namespace HR.LeaveManagement.Persistence.DatabaseContext
 {
     public class HrDatabaseContext : DbContext
     {
-        public HrDatabaseContext(DbContextOptions<HrDatabaseContext> options) : base(options)
-        {
+        private readonly IUserService userService;
 
+        public HrDatabaseContext(DbContextOptions<HrDatabaseContext> options, IUserService userService) : base(options)
+        {
+            this.userService = userService;
         }
 
         public DbSet<LeaveType> LeaveTypes { get; set; }
